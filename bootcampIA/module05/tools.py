2ac48eb7103b5dc60bdf9742b53696fd068a2630
Carolina <cbsomarriba@gmail.com>
2022-12-12 20:06:55 +0100

    tools.py

--- bootcampIA/module05/tools.py
+++ bootcampIA/module05/tools.py
@@ -11,3 +11,19 @@ def add_intercept(x):
     Raises:
     This function should not raise any Exception.
     """
+    X = numpy.ones((lines, columns+1))
+    X[:,:-1] = x #à part la dernière ligne qu'on vient de rajouter, on remplit le reste de la matrice X avec tout ce qui était dans la matrice x
+    print(X)
+
+
+# lst = ([[1, 2, 3, 1, 2, 3, 1, 2, 3],
+#         [1, 2, 3, 1, 2, 3, 1, 2, 3],
+#         [1, 2, 3, 1, 2, 3, 1, 2, 3]]) 
+# #dimensions : 3 x 9 (m = 3, nb de lignes) (n = 9, nb de colonnes)
+# x = numpy.asarray(lst)
+
+lines = 5
+columns = 3
+x = numpy.random.rand(lines,columns) #pour créer une matrice avec numéros random
+#print(x)
+add_intercept(x)
