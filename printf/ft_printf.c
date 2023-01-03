38a0765649b27750cf569ffc3cfd0f84965abd6f
Carolina <cbsomarriba@gmail.com>
2023-01-03 12:58:49 +0100

    sans commentaires

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: casomarr <casomarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: carolina <carolina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 21:06:18 by casomarr          #+#    #+#             */
-/*   Updated: 2023/01/02 21:47:31 by casomarr         ###   ########.fr       */
+/*   Updated: 2023/01/03 12:56:48 by carolina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,10 @@ int	ft_format(int i, const char *arg, va_list ptr)
 	}
 }
 
-int	ft_printf(const char *arg, ...) //attention prototype dans le sujet ne précise pas que le premier argument s'appelle arg!
+int	ft_printf(const char *arg, ...)
 {
-	va_list ptr; // pointer on the first argument (therefore the const char *)
-	va_start (ptr, arg); // pointer on the first argument, argument (on donne à const char * le nom "arg")
+	va_list ptr;
+	va_start (ptr, arg);
 	int i;
 	int length;
 
@@ -48,20 +48,6 @@ int	ft_printf(const char *arg, ...) //attention prototype dans le sujet ne préc
 	length = 0;
 	while(arg[i])
 	{
-		// if (arg[i] == '%' && arg[i + 1] != '\0')
-		// {
-		// 	ft_format(i, arg, ptr);
-		// 	length += ft_format(i, arg, ptr);
-		// 	i++;
-		// }
-		// else
-		// {
-		// 	if (arg[i] == '%' && arg[i + 1] == '\0' )
-		// 		break;
-		// 	length += ft_putchar(arg[i]);
-		// }
-		// i++;
-
 		if (arg[i] != '%')
 			length += ft_putchar(arg[i++]);
 		else
@@ -73,57 +59,3 @@ int	ft_printf(const char *arg, ...) //attention prototype dans le sujet ne préc
 	va_end(ptr);
 	return (length);
 }
-
-// int main()
-// {
-// 	//char *string = "linda";
-// 	// char caractere = 'L';
-// 	// int nb_entier = 1843;
-// 	// long long nb_decimal = 1843;
-// 	// int hexa_min = 1643;
-// 	// int hexa_maj = 1643;
-// 	// char *s1 = "linda";
-// 	// char *s2 = "cómo";
-// 	// char *s3 = "vas?";
-// 	//char *p = "Hola";
-// 	//char *p;
-
-// 	//ft_printf("Hola %s\n", string);
-// 	// ft_printf("Hola %c\n", caractere);
-// 	// ft_printf("Hola %i\n", nb_entier);
-// 	// ft_printf("Hola %d\n", nb_decimal);
-// 	// ft_printf("Hola %x\n", hexa_min);
-// 	// ft_printf("Hola %X\n", hexa_maj);
-// 	// ft_printf("Hola %s %s %s\n", s1, s2, s3);
-// 	// ft_printf("Hola %%%\n");
-// 	//ft_printf("ft_printf0 : %p\n", p);
-// 	//printf("printf : %p\n", p);
-
-// 	// printf ("(%i)\n", printf("Helia"));
-// 	// ft_printf ("(%i)\n", ft_printf("Helia"));
-
-// 	// int i = -1;
-// 	// ft_printf("%d\n", i);
-
-// 	//ft_printf("(%i)\n", ft_printf("hola"));
-
-// 	//int num = 64;
-// 	//ft_printf("(%i)\n", ft_printf("64"));
-
-// 	//ft_printf("(%i)\n", ft_printf("this is a test"));
-
-// 	// ft_printf("%c", 'a');
-// 	// printf("\n%c", 'a');
-
-// 	ft_printf("%s", "");
-
-
-// 	return (0);
-// }
-
-// int main ()
-// {
-// 	//ft_printf("This %p is even stranger\n", (void *)-1);
-// 	ft_printf("(%i)", ft_printf("This %p is even stranger\n", (void *)-1));
-// 	return 0;
-// }
--- printf/is_pointer.c
+++ printf/is_pointer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_pointer.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: casomarr <casomarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: carolina <carolina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:41:45 by casomarr          #+#    #+#             */
-/*   Updated: 2023/01/02 21:57:12 by casomarr         ###   ########.fr       */
+/*   Updated: 2023/01/03 12:58:16 by carolina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,35 +52,6 @@ int ft_nbrlen_unsigned(unsigned long long ptr, int base_len)
 
 int ft_is_pointer(unsigned long long ptr, int trigger)
 {
-	// unsigned long adress;
-	// char *base;
-	// char result[9];
-	// int i;
-
-	// if (ptr == NULL)
-	// {
-	// 	ft_putstr("(nil)");
-	// 	return (5);
-	// }
-	// adress = (unsigned long)ptr;
-	// base = "0123456789abcdef";
-	// i = 8; // 6 numeros + 0x
-	// while ((adress / 16) > 0)
-	// {
-	// 	result[i] = base[(adress % 16)];
-	// 	adress = adress / 16;
-	// 	i--;
-	// }
-	// result[i] = base[(adress % 16)];
-	// ft_putchar('0');
-	// ft_putchar('x');
-	// while(i < 9) // 6 numeros + 0x + \0
-	// {
-	// 	ft_putchar(result[i]);
-	// 	i++;
-	// }
-	// return(ft_nbrlen(ptr, 16) + 2);
-
 	char * base;
 
 	if ((void *)ptr == NULL)
